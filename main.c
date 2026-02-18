d82768102c373349e2d15068c104e19719aebaf2
Jonathan Jacobs <jonathanjacobs2113@gmail.com>
2026-02-18 16:35:40 -0500

    fixed function names

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "input_buffer.h"
+#include "input_processing.h"
 
-int main(void)
+int main(int argc, char** argv)
 {
-    printf("Hello, World!\n");
-    return 0;
+    InputBuffer* input_buff = newInputBuffer();
+    while (true)
+    {
+        printPrompt();
+        readInput(input_buff);
+
+        if (strncmp(input_buff->buffer, ".exit", 26) == 0)
+        {
+            closeInputBuffer(input_buff);
+            exit(EXIT_SUCCESS);
+        }
+
+        else
+        {
+            printf("Invalid Command %s\n", input_buff->buffer);
+        }
+    }
 }
